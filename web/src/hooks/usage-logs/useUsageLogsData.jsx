89a94e6b5f59be63ac03273e9cc3198ca40ceddd
Aureon <mjc1618@163.com>
2025-10-16 18:05:26 +0800

    web: 使用日志中移除请求并计费模型与实际模型模块

--- web/src/hooks/usage-logs/useUsageLogsData.jsx
+++ web/src/hooks/usage-logs/useUsageLogsData.jsx
@@ -22,19 +22,19 @@ import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { ITEMS_PER_PAGE } from '../../constants';
 import {
-    API,
-    copy,
-    getLogOther,
-    getTodayStartTimestamp,
-    isAdmin,
-    renderAudioModelPrice,
-    renderClaudeLogContent,
-    renderClaudeModelPrice,
-    renderLogContent,
-    renderModelPrice,
-    showError,
-    showSuccess,
-    timestamp2string
+  API,
+  copy,
+  getLogOther,
+  getTodayStartTimestamp,
+  isAdmin,
+  renderAudioModelPrice,
+  renderClaudeLogContent,
+  renderClaudeModelPrice,
+  renderLogContent,
+  renderModelPrice,
+  showError,
+  showSuccess,
+  timestamp2string
 } from '../../helpers';
 import { useTableCompactMode } from '../common/useTableCompactMode';
 
@@ -381,20 +381,6 @@ export const useLogsData = () => {
         }
       }
       if (logs[i].type === 2) {
-        let modelMapped =
-          other?.is_model_mapped &&
-          other?.upstream_model_name &&
-          other?.upstream_model_name !== '';
-        if (modelMapped) {
-          expandDataLocal.push({
-            key: t('请求并计费模型'),
-            value: logs[i].model_name,
-          });
-          expandDataLocal.push({
-            key: t('实际模型'),
-            value: other.upstream_model_name,
-          });
-        }
         let content = '';
         if (other?.ws || other?.audio) {
           content = renderAudioModelPrice(
